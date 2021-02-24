0b276cfe9fc03cb3a889bad8f67462237c7466f5
MusaTamzid05 <tamzid101@gmail.com>
2021-02-24 19:17:38 +0600

    not working generator.

--- generator/generator.py
+++ generator/generator.py
@@ -1,4 +1,6 @@
 from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
 import os
 import pickle
 
@@ -7,5 +9,30 @@ class Generator:
     def __init__(self, train_dir_path):
         self.model = load_model(os.path.join(train_dir_path, "model.h5"))
         self.mapping = pickle.load(open(os.path.join(train_dir_path, "mapping.pkl"), "rb"))
+
+
+    def generate(self, src_text, n_chars, seq_length = 10):
+
+        input_text= src_text
+
+        for _ in range(n_chars):
+            encoded = [self.mapping[char] for char in input_text]
+            encoded = pad_sequences([encoded], maxlen = seq_length, truncating = "pre")
+            encoded = to_categorical(encoded, num_classes = len(self.mapping))
+            prediction = self.model.predict_classes(encoded, verbose = 0)
+
+            out_char = ""
+
+            # @TODO : index based mapping should be used
+            for char, index in self.mapping.items():
+                if index == prediction:
+                    out_char = char
+                    print(out_char)
+                    break
+
+            input_text += out_char
+
+        return input_text
+
 
 
--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def train():
 
 def generate_example():
     generator = Generator(train_dir_path = "results")
+    text = generator.generate(src_text = "Sing a song", n_chars = 5)
+    print(text)
 
 
 def main():
